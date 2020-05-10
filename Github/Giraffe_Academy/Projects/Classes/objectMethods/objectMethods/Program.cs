3214b1642b3a7c8bf63619a6ba6454daab1e27cd
ferranarmentera <49804072+ferranarmentera@users.noreply.github.com>
2020-05-10 21:13:47 +0200

    object methods update

---
+++ Github/Giraffe_Academy/Projects/Classes/objectMethods/objectMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace objectMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students student1 = new Students("Jim", "Business", 2.8);
+            Students student2 = new Students("Pam", "Art", 3.6);
+
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Github/Giraffe_Academy/Projects/Classes/objectMethods/objectMethods/Students.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace objectMethods
+{
+    class Students
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Students(string aName, string aMajor, double aGpa)
+        {
+            name = aName;    // si no ho assignem a la variable dona un valor per defecte que no coincideix amb la condició
+                             // en el public Bool 
+            major = aMajor;
+            gpa = aGpa;
+
+        }
+        public bool HasHonors()
+        {
+            if (gpa >= 1.9)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+    }
+    
+      
+}
+

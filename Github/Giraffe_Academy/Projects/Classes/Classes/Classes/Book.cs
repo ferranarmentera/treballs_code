a66fbf987e515a798cf26479c67151c3f695ea92
ferranarmentera <49804072+ferranarmentera@users.noreply.github.com>
2020-05-10 18:51:17 +0200

    update giraffe

---
+++ Github/Giraffe_Academy/Projects/Classes/Classes/Classes/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Book
+    {
+        public string titol;
+        public string autor;
+        public int pagines;
+
+        public Book(string Atitol,string Aautor,int Apagines) // Aquest és el constructor 
+        {
+            titol = Atitol;   // no cal fer l'equivalencia , ho fem per poder ensenyar els dos exemples
+            autor = Aautor;
+            pagines = Apagines;
+}
+    }
+}
---
+++ Github/Giraffe_Academy/Projects/Classes/Classes/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter","JK Row",500);// utilitzem constructors 
+            //book1.titol = "Harry Potter"; // es el mateix , podem crear els objectes ( Book ) de les dos maneres
+            //book1.autor = "JK Rowling";
+            //book1.pagines = 400;
+                                            // podem crear tants book1,2.3,4 com vulguem 
+            Console.WriteLine(book1.pagines);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
